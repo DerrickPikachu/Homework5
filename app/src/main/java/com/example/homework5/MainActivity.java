232d05d19501cae6c417678ab416a44a0c12cdd6
pikaChin <derrfgh4563@gmail.com>
2019-11-28 15:57:47 +0800

    intent call activity complete

--- app/src/main/java/com/example/homework5/MainActivity.java
+++ app/src/main/java/com/example/homework5/MainActivity.java
@@ -4,9 +4,15 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.preference.PreferenceManager;
 import android.view.View;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+
+    private TextView txv;
+    private String record;
+    public static final int CALL_ACTIVITY = 1;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -15,17 +21,42 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
 
         findViewById(R.id.intentBtn).setOnClickListener(this);
         findViewById(R.id.goBackBtn).setOnClickListener(this);
+
+        txv = findViewById(R.id.visitedRecord);
+
+        Intent father = getIntent();
+        String preVisited = father.getStringExtra("record");
+        if (preVisited == null) {
+            record = "1";
+        }
+        else {
+            record = preVisited + "->1";
+        }
+        txv.setText(record);
     }
 
     @Override
     public void onClick(View v) {
+        Intent toActivity = new Intent();
+
         if (v.getId() == R.id.goBackBtn) {
+            toActivity.putExtra("record", record);
+            setResult(RESULT_OK, toActivity);
             finish();
         }
         else if (v.getId() == R.id.intentBtn) {
-            Intent toActivity = new Intent();
             toActivity.setClass(this, SecondActivity.class);
-            startActivity(toActivity);
+            toActivity.putExtra("record", record);
+            startActivityForResult(toActivity, CALL_ACTIVITY);
+        }
+    }
+
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        if (requestCode == CALL_ACTIVITY) {
+            record = data.getStringExtra("record") + "->1";
+            txv.setText(record);
         }
     }
 }
--- app/src/main/java/com/example/homework5/ThirdActivity.java
+++ app/src/main/java/com/example/homework5/ThirdActivity.java
@@ -5,8 +5,14 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.TextView;
+
+import static com.example.homework5.MainActivity.CALL_ACTIVITY;
 
 public class ThirdActivity extends AppCompatActivity implements View.OnClickListener {
+
+    private TextView txv;
+    private String record;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -15,17 +21,36 @@ public class ThirdActivity extends AppCompatActivity implements View.OnClickList
 
         findViewById(R.id.intentBtn).setOnClickListener(this);
         findViewById(R.id.goBackBtn).setOnClickListener(this);
+
+        txv = findViewById(R.id.visitedRecord);
+
+        Intent father = getIntent();
+        record = father.getStringExtra("record");
+        record = record + "->3";
+        txv.setText(record);
     }
 
     @Override
     public void onClick(View v) {
+        Intent toActivity = new Intent();
+
         if (v.getId() == R.id.goBackBtn) {
+            toActivity.putExtra("record", record);
+            setResult(RESULT_OK, toActivity);
             finish();
         }
         else if (v.getId() == R.id.intentBtn) {
-            Intent toActivity = new Intent();
             toActivity.setClass(this, MainActivity.class);
-            startActivity(toActivity);
+            toActivity.putExtra("record", record);
+            startActivityForResult(toActivity, CALL_ACTIVITY);
+        }
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        if (requestCode == CALL_ACTIVITY) {
+            record = data.getStringExtra("record") + "->3";
+            txv.setText(record);
         }
     }
 }
